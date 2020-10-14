4df4b752097d8a2b3644c481d63c0625be12196f
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-14 03:58:23 -0400

    added potions object

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,15 @@
+test('creates a health potion object', () => {
+    // new keyword creates a new object
+    const potion = new Potion('health')
+
+    expect(potion.name).toBe('health')
+    expect(potion.value).toEqual(expect.any(Number))
+})
+test('ceates a random potion object', () => {
+    const potion = new Potion()
+    expect(potion.name).toEqual(expect.any(String))
+    expect(potion.name.length).toBeGreaterThan(0)
+    expect(potion.value).toEqual(expect.any(Number))
+})
+
+const Potion = require('../lib/Potion')
